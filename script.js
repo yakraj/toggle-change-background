94fa139cb7bd076c3af22ed67a7f979a66035c18
Yakraj Pariyar <princeyakraj289@gmail.com>
2021-03-31 06:21:44 +0530

    first

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var element = document.getElementById("toggle");
+    var element1 = document.getElementById("background");
+    element.classList.toggle("newoutliner");
+    element1.classList.toggle("secondnight");
+   
+ }
